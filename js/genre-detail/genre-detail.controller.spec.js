8cb5efd6dc32ee8e61edcf8d4ea4923be3de9b3b
dtrinh888 <dtrinh888@gmail.com>
2016-02-29 20:08:12 -0800

    genre detail route and controller test successful

---
+++ js/genre-detail/genre-detail.controller.spec.js
@@ -0,0 +1,9 @@
+describe('Genre Detail Controller', function(){
+	var ctrl, scope, $location, GenreDetailCtrl;
+
+	beforeEach(module('MovieFinder'));
+
+	it('should route to movie when selectMovie is called', inject(function($httpBackend, $rootScope, $route, $location){
+
+	}));
+});
